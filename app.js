7601d02df462ae50e8d7c896fd85b530a407b14e
Jason Sherrick <jdev@DEB-PC.home>
2023-10-09 21:49:07 -0500

    removed redundant express/response import

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const res = require('express/lib/response');
 const app = express();
 const path = require('path');
-const campgrounds = require('./controllers/campground-controller');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+const campgrounds = require('./controllers/campground-controller');
 const db = require("./db-connect");
 db.sequelize.sync();
 
