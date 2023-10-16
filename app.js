8de85c68933a69517eb7a6edaa3b7f98d2bfb6af
Jason Sherrick <jdev@DEB-LT.home>
2023-10-16 13:28:30 -0500

    finished seed modules and sseed database with dummy data

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const path = require('path');
-const mongoose = require('mongoose');
+const db = require('./controllers/db-connect');
+const seed = require('./seed-data/seed');
+const { error } = require('console');
+
+db()
+    .then(() => {
+        console.log('db is connected');
+    })
+    .catch((error) => {
+        console.error.apply(`db had an error: ${error}`);
+    })
+
+seed()
+    .then(() => {
+        console.log('db got seeded');
+    })
+    .catch((err) => {
+        console.error(`seeding ran into a problem: ${err}`);
+    })
 
 const app = express();
 
