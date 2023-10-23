66975518bbe8734287fab2fd5d365ad0a80c1ee1
Jason Sherrick <jdev@DEB-LT.home>
2023-10-23 15:39:35 -0500

    enabled user authentication in Mongo and added env variables

--- controllers/db-connect.js
+++ controllers/db-connect.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
+require("dotenv").config();
+
+const { DB_USER, DB_PASS, DB_NAME, DB_HOST } = process.env;
 
 async function connectDb() {
-    await mongoose.connect('mongodb://localhost:27017/yelp-camp');
+    await mongoose.connect(`mongodb://${DB_USER}:${encodeURIComponent(DB_PASS)}@${DB_HOST}:27017/${DB_NAME}`);
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error'));
     db.once('open', () => {
