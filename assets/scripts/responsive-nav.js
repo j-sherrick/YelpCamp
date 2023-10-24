2b8541c9963d21c4c9f37c3010061d815c6acafa
Jason Sherrick <jdev@DEB-LT.home>
2023-10-24 14:32:57 -0500

    added nav togle

--- assets/scripts/responsive-nav.js
+++ assets/scripts/responsive-nav.js
@@ -1,9 +1,15 @@
-const addClickListener = (item) => {
-    document.querySelector()
+const hamburger = document.querySelector('.navigation__hamburger');
+hamburger.addEventListener('click', showNavigation);
+
+function showNavigation() {
+    const links = document.querySelector('.navigation__links');
+    console.log(links);
+    if (links.classList.contains('navigation__links--collapsed')) {
+        console.log(`Removing collapsed nav class`);
+        links.classList.remove('navigation__links--collapsed');
+    }
+    else {
+        console.log(`Adding collapsed nav class`);
+        links.classList.add('navigation__links--collapsed');
+    }
 }
-
-const showNavigation = (domElement) => {
-
-}
-
-module.exports = { addClickListener, showNavication: showNavigation }
