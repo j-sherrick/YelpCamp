2c98d7ff6a9fd29d8ad5fc8c453b1420ef917adb
Jason Sherrick <jdev@DEB-LT.home>
2023-10-24 08:31:47 -0500

    added styling and begin responsive nave

---
+++ assets/scripts/responsive-nav.js
@@ -0,0 +1,9 @@
+const addClickListener = (item) => {
+    document.querySelector()
+}
+
+const showNavigation = (domElement) => {
+
+}
+
+module.exports = { addClickListener, showNavication: showNavigation }
